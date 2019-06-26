4362aabe6e396a673b85d72762cd93b68f8ecb9e
beatrice_gaube <beatrice.gaube@assist.ro>
2019-06-26 15:46:27 +0300

    Login and Splash Screen Implementation

--- TestKoinMVVM/app/src/main/java/com/example/testkoinmvvm/app/di/AppModule.kt
+++ TestKoinMVVM/app/src/main/java/com/example/testkoinmvvm/app/di/AppModule.kt
@@ -1,6 +1,7 @@
 package com.example.testkoinmvvm.app.di
 
-import com.example.testkoinmvvm.activities.splash.SplashViewModel
+import com.example.testkoinmvvm.fragments.login.LoginViewModel
+import com.example.testkoinmvvm.fragments.splash.SplashViewModel
 import org.koin.android.viewmodel.dsl.viewModel
 import org.koin.core.module.Module
 import org.koin.dsl.module
@@ -9,4 +10,6 @@ import org.koin.dsl.module
 val appModule: Module = module {
 
     viewModel { SplashViewModel() }
+    viewModel { LoginViewModel() }
+
 }
